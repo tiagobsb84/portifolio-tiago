247d0dd9be0bf5b22bec43f2d40612fff455e1c2
Tiago Silva <tiagobsb84@gmail.com>
2024-12-03 16:14:34 -0300

    scroll do navegado active no js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,51 @@
 /* BUTTON ACTIVE NAV */
 const itemNavigation = document.querySelectorAll('.nav__list li a');
+const sections = document.querySelectorAll('section');
+
 
 itemNavigation.forEach(item => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();//evita o comportamento padrão.
+        const targetId = item.getAttribute('href').substring(1); //Obtém o ID da seção.
+        const targetSelection = document.getElementById(targetId);
+
+        //Faz scroll suave até a seção clicada.
+        window.scrollTo({
+            top: targetSelection.offsetTop - 50, //Ajuste para compensa o menu fixo.
+            behavior: 'smooth'
+        })
+
         // Remove a classe 'active' de todos os itens
         itemNavigation.forEach(nav => nav.classList.remove('active-link'));
          // Adiciona a classe 'active' no item clicado
         item.classList.add('active-link');
     })
+});
+
+//Adicione evento de scroll para atualizar a classe 'active'.
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    //Identifica qual seção está no centro da tela.
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if(window.scrollY >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    //Atualiza o menu para destacar a seção atual sem interferir no clique.
+    if(current) {
+        itemNavigation.forEach(item => {
+            if(item.getAttribute('href').substring(1) === current) {
+                item.classList.add('active-link');
+            } else {
+                item.classList.remove('active-link');
+            }
+        })
+    }
 })
 
 /*const prevButton = document.querySelector('.carousel__button--prev');
