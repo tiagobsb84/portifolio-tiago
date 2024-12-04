c14b91e17e6a2918bfffa1dbf1b400935363a71d
Tiago Silva <tiagobsb84@gmail.com>
2024-12-04 07:01:24 -0300

    favicon, e o carrossel do projeto

---
+++ assets/js/carousel.js
@@ -0,0 +1,30 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // Paginação bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsivo breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
--- assets/js/main.js
+++ assets/js/main.js
@@ -47,31 +47,3 @@ window.addEventListener('scroll', () => {
         })
     }
 })
-
-/*const prevButton = document.querySelector('.carousel__button--prev');
-const nextButton = document.querySelector('.carousel__button--next');
-const carouselContainer = document.querySelector('.project__carousel__container');
-
-let currentIndex = 0;
-
-nextButton.addEventListener('click', () => {
-    const totalCards = document.querySelectorAll('.project__card').length;
-    const visibleCards = 3;
-    if(currentIndex < totalCards - visibleCards) {
-        currentIndex++;
-        updateCarousel();
-    }
-});
-
-prevButton.addEventListener('click', () => {
-    if(currentIndex > 0) {
-        currentIndex--;
-        updateCarousel();
-    }
-});
-
-//offsetWidth retorna a largura total de um elemento em pixels, padding e bodar. Não inclui margin.
-function updateCarousel() {
-    const cardWidth = document.querySelector('.project__card').offsetWidth;
-    carouselContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-}*/
